98d77369841b69aad6c7c898ab980d151b144580
anonymous <anonymous>
2016-11-06 12:10:08 -0500

    Switched to in-place translation of the strings
    This avoids the unnecessarily complex memory management scheme I had been contemplating.

--- librpn.c
+++ librpn.c
@@ -9,33 +9,17 @@
  * An additional character is needed for a terminating NULL value.
  */
 #define RPN_BUFFER_SIZE 52
-#define RPN_BUFFER_COUNT 50
 
-char arena[RPN_BUFFER_COUNT][RPN_BUFFER_SIZE];
-int arena_idx = 0;
+char *infix_to_rpn(char *infix) {
+  char buffer[RPN_BUFFER_SIZE];
+  memset(buffer, 0, RPN_BUFFER_SIZE);
 
-void rpn_init() {
-  memset(arena, 0, RPN_BUFFER_SIZE * RPN_BUFFER_COUNT);
-  arena_idx = 0;
-}
+  buffer[0] = infix[0];
+  buffer[1] = infix[2];
+  buffer[2] = '+';
 
-void rpn_release() {
-  // Overwriting memory because it can prevent false positives in the event of
-  // bad memory handling.
-  memset(arena, 0, RPN_BUFFER_SIZE * RPN_BUFFER_COUNT);
-}
-
-char *arena_get_next() {
-  if (arena_idx < RPN_BUFFER_COUNT) {
-    return (char *)&arena[arena_idx++];
-  }
-  return NULL;
-}
-
-char *infix_to_rpn(const char *infix) {
-    char *buffer;
-
-    buffer = arena_get_next(); 
-    strcpy(buffer, "ab+");
-    return buffer; 
+  // Unchecked copy is safe because the RPN representation will always be the
+  // same size or smaller than the infix notation.
+  strcpy(infix, buffer);
+  return infix;
 }
--- librpn.h
+++ librpn.h
@@ -1,9 +1,6 @@
 #ifndef _UNIT_H_
 #define _UNIT_H_
 
-void rpn_init();
-void rpn_release();
-
-char* infix_to_rpn(const char* infix);
+char* infix_to_rpn(char* infix);
 
 #endif
